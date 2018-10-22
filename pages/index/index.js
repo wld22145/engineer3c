4b3ae59a1c634c3f3dece4f0607754b67d0f7c6f
WangLingDong <wld22145@126.com>
2018-10-22 18:55:19 +0800

    live pusher

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,12 @@
 const app = getApp()
 
 Page({
+  statechange(e) {
+    console.log('live-player code:', e.detail.code)
+  },
+  error(e) {
+    console.error('live-player error:', e.detail.errMsg)
+  },
   data: {
     motto: 'Hello World',
     userInfo: {},
