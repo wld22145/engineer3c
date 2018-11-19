4a865c7df43de9ebf21a463a27edb287d1a9a321
Duan <1570005763@qq.com>
2018-11-20 00:12:43 +0800

    新建utils文件夹
    存放常用js文件

---
+++ miniprogram/utils/hostFunction.js
@@ -0,0 +1,36 @@
+const db = wx.cloud.database({ env: "test-d49d77" });
+
+function goLeft() {
+  insertInstruction("left");
+}
+
+function goRight() {
+  insertInstruction("right");
+}
+
+function goForward() {
+  insertInstruction("forward");
+}
+
+function goBackward() {
+  insertInstruction("backward");
+}
+
+function insertInstruction(instruction) {
+  return db.collection('instructions').add({
+    data: {
+      instruction: instruction,
+      time: new Date(),
+    }
+  })
+    .then(function (res) {
+      console.log(res)
+    })
+}
+
+module.exports = {
+  goLeft: goLeft,
+  goRight:goRight,
+  goForward: goForward,
+  goBackward: goBackward
+}
---
+++ miniprogram/utils/slaveFunction.js
@@ -0,0 +1,53 @@
+const db = wx.cloud.database({ env: "test-d49d77" });
+var latestInstruction;
+var latestIndex = 0;
+var latestTime;
+var instruction;
+
+// get earliest instruction
+function getInstruction() {
+  return new Promise(function (resolve, reject) {
+    db.collection("instructions").orderBy('time', 'asc').limit(1).get().then(res => {
+      resolve(res.data)
+    })
+  })
+}
+
+// remove it from database
+function removeInstruction(input) {
+  return new Promise(function (resolve, reject) {
+    console.log(input)
+    const targetId = input[0]["_id"]
+    db.collection('instructions').doc(targetId).remove()
+    resolve(input[0])
+  })
+}
+
+// operate the instruction
+// remain fulfilling
+function operateInstruction(instruction) {
+  console.log(instruction)
+
+}
+
+// listen once 
+function listen() {
+  getInstruction().then(removeInstruction).then(function (result) {
+    operateInstruction(result);
+  }).catch(e => { console.error(e) });
+}
+
+//keep listening
+function startListening() {
+  listen()
+  setTimeout(startListening, 500)
+}
+
+
+module.exports = {
+  getInstruction: getInstruction,
+  removeInstruction: removeInstruction,
+  operateInstruction: operateInstruction,
+  listen: listen,
+  startListening: startListening
+}
