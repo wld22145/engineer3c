7d97dcab7f46ec484497a608b80837ce1d76ffd9
zyy <unknown@example.com>
2018-11-22 14:13:47 +0800

    四个方向控制

---
+++ miniprogram/pages/fourdirections/fourdirections.js
@@ -0,0 +1,14 @@
+Page({
+  ahead: function () {
+    console.log("go ahead")
+  },
+  left: function () {
+    console.log("go left")
+  },
+  right: function () {
+    console.log("go right")
+  },
+  back: function () {
+    console.log("go back")
+  }
+})
