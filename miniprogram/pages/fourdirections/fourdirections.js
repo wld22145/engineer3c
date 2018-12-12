b134d7582d5b7559b7dba99490fc112081e83f40
WangLingDong <wld22145@126.com>
2018-12-12 14:28:27 +0800

    bluetooth
    wld commit

--- miniprogram/pages/fourdirections/fourdirections.js
+++ miniprogram/pages/fourdirections/fourdirections.js
@@ -1,14 +1,27 @@
+//index.js
+
+var bluetoothFunction = require('../../utils/bluetoothFunction.js');
+
 Page({
   ahead: function () {
     console.log("go ahead")
+    bluetoothFunction.goAhead()
   },
   left: function () {
     console.log("go left")
+    bluetoothFunction.goLeft()
   },
   right: function () {
     console.log("go right")
+    bluetoothFunction.goRight()
   },
   back: function () {
     console.log("go back")
+    bluetoothFunction.goBack()
+  },
+  stop: function() {
+    console.log("stop")
+    bluetoothFunction.stop()
   }
+  
 })
